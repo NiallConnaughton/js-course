631b25efb7ac47387c11bd1547a54ba0ddaee655
Niall Connaughton <nconnaughton@gmail.com>
2014-08-11 20:37:18 +1000

    nasty first implementation of placing pieces on the board

--- Week 5/js/ticTacToe.js
+++ Week 5/js/ticTacToe.js
@@ -1,6 +1,7 @@
 console.log('Tic Tac Toe in Javascript');
 
-var CELL_EMPTY = 0, PLAYER_1 = 1, PLAYER_2 = 2;
+var CELL_EMPTY = -1, PLAYER_1 = 0, PLAYER_2 = 1;
+var current_player = PLAYER_1;
 var grid = [];
 
 function initializeGrid() {
@@ -12,13 +13,39 @@ function initializeGrid() {
 			row.push(CELL_EMPTY);
 
 			var cellId = getCellId(j, i);
-			displayGrid.push('<div class="cell" id="' + cellId + '"> </div>');
+			var buttonId = 'button' + cellId;
+
+			var cellHtml = '<div class="cell" id="' + cellId + '"><button id="' + buttonId + '" class="cellButton">foo</button> </div>';
+			displayGrid.push(cellHtml);
 		}
 		grid.push(row);
 	}
 
 	var renderedContent = displayGrid.join('\n');
 	$('#board').html(renderedContent);
+
+
+	// for (var i = 0; i < 3; i++) {
+	// 	for (var j = 0; j < 3; j++) {
+	// 		cellId = getCellId(i, j);
+	// 		button = $('#' + cellId + ' .cellButton');
+
+	// 		button.click(function(x, y) {
+	// 			console.log('clicked ' + x + ', ' + y);
+	// 			placePiece(x, y, PLAYER_1);
+	// 		}(i, j));
+	// 	}
+	// }
+	var buttons = $('#board').find('.cellButton');
+
+	buttons.click(function() {
+		var clickedCell = $(this).parent().attr('id');
+		var x = clickedCell[1];
+		var y = clickedCell[3];
+
+		placePiece(x, y, current_player);
+		current_player = (current_player + 1) % 2;
+	})
 }
 
 function placePiece(x, y, player) {
@@ -41,5 +68,5 @@ function getCellId(x, y) {
 
 initializeGrid();
 
-placePiece(1, 1, PLAYER_1);
-placePiece(1, 2, PLAYER_2);
+//placePiece(1, 1, PLAYER_1);
+//placePiece(1, 2, PLAYER_2);
