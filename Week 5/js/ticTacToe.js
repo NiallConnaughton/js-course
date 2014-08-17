5a176ad501d0d611c24a4f9b5bce224c777b2499
Niall Connaughton <nconnaughton@gmail.com>
2014-08-17 12:55:38 +1000

    First cut of end of game logic

--- Week 5/js/ticTacToe.js
+++ Week 5/js/ticTacToe.js
@@ -1,8 +1,9 @@
 console.log('Tic Tac Toe in Javascript');
 
-var CELL_EMPTY = -1, PLAYER_1 = 0, PLAYER_2 = 1;
+var PLAYER_1 = 0, PLAYER_2 = 1, CELL_EMPTY = undefined;
 var current_player = PLAYER_1;
 var grid = [];
+var moves = 0;
 
 function initializeGrid() {
 	var displayGrid = [];
@@ -44,7 +45,6 @@ function initializeGrid() {
 		var y = clickedCell[3];
 
 		placePiece(x, y, current_player);
-		current_player = (current_player + 1) % 2;
 	})
 }
 
@@ -60,6 +60,44 @@ function placePiece(x, y, player) {
 		cell.html('X');
 	else if (player === PLAYER_2)
 		cell.html('O');
+
+	moves++;
+	current_player = (current_player + 1) % 2;
+
+	checkGameState();
+}
+
+function checkGameState() {
+	var consecutiveCellSets = [];
+	
+	// each column
+	consecutiveCellSets.push(grid[0]);
+	consecutiveCellSets.push(grid[1]);
+	consecutiveCellSets.push(grid[2]);
+
+	consecutiveCellSets.push([grid[0][0], grid[1][0], grid[2][0]]);
+	consecutiveCellSets.push([grid[0][1], grid[1][1], grid[2][1]]);
+	consecutiveCellSets.push([grid[0][2], grid[1][2], grid[2][2]]);
+
+	for (var i = 0; i < consecutiveCellSets.length; i++) {
+		var consecutiveSet = consecutiveCellSets[i];
+		var sum = 0;
+		for (var cell = 0; cell < 3; cell++) {
+			sum += consecutiveSet[cell];
+		}
+
+		console.log(sum);
+
+		if (sum === PLAYER_1 * 3) {
+			alert('player 1 win!');
+		}
+		else if (sum === PLAYER_2 * 3) {
+			alert('player 2 win!');
+		}
+	}
+
+	if (moves === 9)
+		alert('tie!');
 }
 
 function getCellId(x, y) {
@@ -68,5 +106,8 @@ function getCellId(x, y) {
 
 initializeGrid();
 
-//placePiece(1, 1, PLAYER_1);
-//placePiece(1, 2, PLAYER_2);
+// placePiece(0, 0, PLAYER_1);
+// placePiece(0, 1, PLAYER_1);
+// placePiece(0, 2, PLAYER_1);
+
+// checkGameState();
