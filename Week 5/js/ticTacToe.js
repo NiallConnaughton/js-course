7db9b4e355b197984bac271a655c1959011ce045
Niall Connaughton <nconnaughton@gmail.com>
2014-08-17 14:09:36 +1000

    Using html grid instead of generating in js. Added diagonals to possible wins. Removed buttons from divs.

--- Week 5/js/ticTacToe.js
+++ Week 5/js/ticTacToe.js
@@ -1,65 +1,52 @@
-console.log('Tic Tac Toe in Javascript');
-
 var PLAYER_1 = 0, PLAYER_2 = 1, CELL_EMPTY = undefined;
 var current_player = PLAYER_1;
 var grid = [];
 var moves = 0;
 
 function initializeGrid() {
-	var displayGrid = [];
-
+	// initialize the game board model
 	for (var i = 0; i < 3; i++) {
 		var row = [];
 		for (var j = 0; j < 3; j++) {
 			row.push(CELL_EMPTY);
-
-			var cellId = getCellId(j, i);
-			var buttonId = 'button' + cellId;
-
-			var cellHtml = '<div class="cell" id="' + cellId + '"><button id="' + buttonId + '" class="cellButton">foo</button> </div>';
-			displayGrid.push(cellHtml);
 		}
 		grid.push(row);
 	}
 
-	var renderedContent = displayGrid.join('\n');
-	$('#board').html(renderedContent);
+	var cells = $('#board').find('.cell');
 
-
-	// for (var i = 0; i < 3; i++) {
-	// 	for (var j = 0; j < 3; j++) {
-	// 		cellId = getCellId(i, j);
-	// 		button = $('#' + cellId + ' .cellButton');
-
-	// 		button.click(function(x, y) {
-	// 			console.log('clicked ' + x + ', ' + y);
-	// 			placePiece(x, y, PLAYER_1);
-	// 		}(i, j));
-	// 	}
-	// }
-	var buttons = $('#board').find('.cellButton');
-
-	buttons.click(function() {
-		var clickedCell = $(this).parent().attr('id');
-		var x = clickedCell[1];
-		var y = clickedCell[3];
+	// handle click events on any of the cells, and place a piece there
+	cells.click(function() {
+		var x = $(this).attr('data-x');
+		var y = $(this).attr('data-y');
 
 		placePiece(x, y, current_player);
-	})
+	});
+
+	// every cell starts empty
+	cells.addClass('emptyCell');
 }
 
 function placePiece(x, y, player) {
+	// var cellId = getCellId(x, y);
+	var selector = '[data-x=' + x + '][data-y=' + y + ']';
+	var cell = $(selector);
+
+	// if the cell has already been played, do nothing
+	if (!cell.hasClass('emptyCell'))
+		return;
+
+	// otherwise, we need to update our board model, set the class for the cell and dispaly the X/O content
 	grid[x][y] = player;
 
-	var cellId = getCellId(x, y);
-	var cell = $('#' + cellId);
 	var cellClass = 'player' + player;
-
 	cell.addClass(cellClass);
 	if (player === PLAYER_1)
 		cell.html('X');
 	else if (player === PLAYER_2)
 		cell.html('O');
+
+	cell.removeClass('emptyCell');
 
 	moves++;
 	current_player = (current_player + 1) % 2;
@@ -75,9 +62,14 @@ function checkGameState() {
 	consecutiveCellSets.push(grid[1]);
 	consecutiveCellSets.push(grid[2]);
 
+	// each row
 	consecutiveCellSets.push([grid[0][0], grid[1][0], grid[2][0]]);
 	consecutiveCellSets.push([grid[0][1], grid[1][1], grid[2][1]]);
 	consecutiveCellSets.push([grid[0][2], grid[1][2], grid[2][2]]);
+
+	// the two diagonals
+	consecutiveCellSets.push([grid[0][0], grid[1][1], grid[2][2]]);
+	consecutiveCellSets.push([grid[2][0], grid[1][1], grid[0][2]]);
 
 	for (var i = 0; i < consecutiveCellSets.length; i++) {
 		var consecutiveSet = consecutiveCellSets[i];
@@ -100,14 +92,4 @@ function checkGameState() {
 		alert('tie!');
 }
 
-function getCellId(x, y) {
-	return 'x' + x + 'y' + y;
-}
-
 initializeGrid();
-
-// placePiece(0, 0, PLAYER_1);
-// placePiece(0, 1, PLAYER_1);
-// placePiece(0, 2, PLAYER_1);
-
-// checkGameState();
