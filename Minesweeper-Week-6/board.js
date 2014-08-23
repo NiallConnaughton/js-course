220f6e3d70c3aaaf10b07b6fadbd4b668c5dc1db
Niall Connaughton <nconnaughton@gmail.com>
2014-08-23 23:31:14 +1000

    Identifying neighbour cells and incrementing count of neighbouring bombs

--- Minesweeper-Week-6/board.js
+++ Minesweeper-Week-6/board.js
@@ -14,6 +14,8 @@ Board.prototype.initializeGrid = function() {
 		this.cells.push(row);
 	}
 
+	console.log(this.cells[2]);
+
 	this.placeBombs();
 
 	console.log('Grid initialized');
@@ -23,16 +25,47 @@ Board.prototype.placeBombs = function() {
 	for (var i = 0; i < this.bombCount; i++) {
 		var bombPlaced = false;
 		while (!bombPlaced) {
-			var x = Math.floor(Math.random() * this.size + 1);
-			var y = Math.floor(Math.random() * this.size + 1);
+			var x = Math.floor(Math.random() * this.size);
+			var y = Math.floor(Math.random() * this.size);
 
 			var cell = this.cells[x][y];
+			console.log('Cell: ' + cell);
 			if (!cell.isBomb) {
 				cell.isBomb = true;
 				bombPlaced = true;
 
-				console.log('Bomb at ' + x + ', ' + y);
+				// console.log('Bomb at ' + x + ', ' + y);
+
+				var neighbours = this.getNeighbours(cell);
+				console.log(neighbours);
 			}
 		}
 	}
+}
+
+Board.prototype.getNeighbours = function(cell) {
+	// neighbours are the cells from one column left, one row above to one column right, one row below the cell
+	// but we need to handle the case where the cell is on the topmost/leftmost/rightmost/bottommost row/column
+
+	var left = Math.max(cell.x - 1, 0);
+	var top = Math.max(cell.y - 1, 0);
+	var right = Math.min(cell.x + 1, this.size);
+	var bottom = Math.min(cell.y - 1, this.size);
+
+	var neighbours = [];
+
+	// console.log(this);
+
+	for (var x = left; x <= right; x++) {
+		for (var y = top; y <= bottom; y++) {
+			// make sure the cell is not its own neighbour
+			if (x === cell.x && y === cell.y) {
+				continue;
+			}
+
+			neighbours.push(this.cells[x][y]);
+		}
+	}
+
+	return neighbours;
 }
--- Minesweeper-Week-6/cell.js
+++ Minesweeper-Week-6/cell.js
@@ -1,4 +1,12 @@
 function Cell(x, y) {
+	this.x = x;
+	this.y = y;
+
 	this.isBomb = false;
 	this.isFlagged = false;
+	this.neighbourBombs = 0;
+}
+
+function addNeighbourBomb() {
+	this.neighbourBombs++;
 }
