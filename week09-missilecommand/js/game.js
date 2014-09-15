9d7714cd67b8ec060b67458f4f3661dc36210fa8
Niall Connaughton <nconnaughton@gmail.com>
2014-09-15 19:20:49 +1000

    Creating initial model objects

--- week09-missilecommand/js/game.js
+++ week09-missilecommand/js/game.js
@@ -4,8 +4,6 @@ var ctx = canvas.getContext('2d');
 var img = new Image();
 img.src = 'images/ground.png';
 img.onload = function() {
-	console.log('foo');
-
 	var groundPattern = ctx.createPattern(img,'repeat-x');
 	ctx.fillStyle = groundPattern;
 	ctx.translate(0, 495);
