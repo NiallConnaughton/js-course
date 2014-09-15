0ad8ce8712a0d38b1ac961c0157a0f65061a53be
Niall Connaughton <nconnaughton@gmail.com>
2014-09-15 20:08:41 +1000

    Rendering cities

--- week09-missilecommand/js/game.js
+++ week09-missilecommand/js/game.js
@@ -4,20 +4,42 @@ var ctx = canvas.getContext('2d');
 function Game() {
 	this.renderer = new Renderer(this);
 	this.cities = [];
+	this.bunkers = [];
+	this.enemyMissiles = [];
+	this.defenseMissiles = [];
+	this.explosions = [];
 }
 
 Game.prototype.initialize = function() {
-	var city = new City(20, 650);
+	var city = new City(20, 450);
 	this.cities.push(city);
+
+	var missile = new Missile(10, 0, 50, 550);
+	this.enemyMissiles.push(missile);
+
+	var defenseMissile = new Missile(30, 500, 80, 0);
+	this.defenseMissiles.push(defenseMissile);
+
+	var bunker = new Bunker(10, 500);
+	this.bunkers.push(bunker);
+
+	var unexplainedExplosion = new Explosion(80, 80);
+	this.explosions.push(unexplainedExplosion);
+
+	this.render();
 }
 
-Game.prototype.onRenderRequest = function (timestamp) {
+Game.prototype.render = function (timestamp) {
 	console.log('rendering' + this);
 	this.renderer.render();
 }
 
+Game.prototype.requestAnimationFrame = function() {
+	var self = this;
+	window.requestAnimationFrame(function(t) {
+		self.render.call(self, t);
+	});
+}
 
 var game = new Game();
-window.requestAnimationFrame(function(t) {
-	game.onRenderRequest.call(game, t);
-});
+game.initialize();
--- week09-missilecommand/js/renderer.js
+++ week09-missilecommand/js/renderer.js
@@ -3,16 +3,35 @@ function Renderer(game) {
 }
 
 Renderer.prototype.render = function() {
+	var self = this;
 	this.renderGround();
+
+	this.game.cities.forEach(function(city) { self.renderCity.call(self, city); });
 }
 
 Renderer.prototype.renderGround = function() {
 	var groundImg = new Image();
 	groundImg.src = 'images/ground.png';
 	groundImg.onload = function() {
+		ctx.save();
 		var groundPattern = ctx.createPattern(groundImg, 'repeat-x');
 		ctx.fillStyle = groundPattern;
 		ctx.translate(0, 495);
 		ctx.fillRect(0, 0, 800, 105);
+		ctx.restore();
+	}
+}
+
+Renderer.prototype.renderCity = function(city) {
+	console.log(this);
+	this.renderImage('images/city.png', city);
+}
+
+Renderer.prototype.renderImage = function(url, gameObject) {
+	// Should change this to pass in preloaded images
+	var img = new Image();
+	img.src = url;
+	img.onload = function() {
+		ctx.drawImage(img, gameObject.x, gameObject.y);
 	}
 }
